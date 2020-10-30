aade43daa57d85023ae6abbcfb62b94d9fc5ea33
Administrador <Administrador@DESKTOP-FRBLCPD>
2020-10-30 18:49:46 -0300

    Creacion proyecto, paquetes inciales y dependencia validation
    Paquetes model, controller y validation

---
+++ AdoptaTuBadBunny/src/main/java/cl/tswoo/adoptatubadbunnyAPP/AdoptaTuBadBunnyApplication.java
@@ -0,0 +1,13 @@
+package cl.tswoo.adoptatubadbunnyAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaTuBadBunnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaTuBadBunnyApplication.class, args);
+	}
+
+}
---
+++ AdoptaTuBadBunny/src/test/java/cl/tswoo/adoptatubadbunnyAPP/AdoptaTuBadBunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tswoo.adoptatubadbunnyAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaTuBadBunnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
